1756b546a31a735c608968be5892f567643564a2
Laio Gabriel <laiogabriel110604@gmail.com>
2026-01-09 12:24:33 -0300

    teste final do layout

--- app/.components/navbar.tsx
+++ app/.components/navbar.tsx
@@ -7,18 +7,18 @@ export default function Navbar({ title }: NavbarProps) {
   return (
     <nav className="
   sticky top-0 z-50
-  bg-slate-950/80 backdrop-blur-md
-  border-b border-white/5
+  bg-white/80 backdrop-blur-md
+  border-b border-slate-200
 ">
       <div className="flex items-center justify-between px-6 py-4">
         <div className="flex items-center gap-2">
-          <h1 className="text-xl font-semibold text-yellow-400">
+          <h1 className="text-xl font-semibold text-[#073175]">
             {title}
           </h1>
-          <IconMovie className="text-yellow-400" />
+          <IconMovie className="text-[#073175]" />
         </div>
 
-        <ul className="flex gap-6 text-slate-200">
+        <ul className="flex gap-6 text-slate-600">
           <li className="relative group cursor-pointer">
             <a href="/">
               <IconHome />
@@ -26,7 +26,7 @@ export default function Navbar({ title }: NavbarProps) {
             <span className="
           absolute left-0 -bottom-1
           h-0.5 w-0
-          bg-red-500
+          bg-[#073175]
           transition-all duration-300
           group-hover:w-full
         " />
--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import MovieList from "./.components/movieList";
 export default function Home() {
 
   return (
-    <>  
+    <main className="min-h-screen">  
     <Navbar title="Cineminha" />
     <MovieList />
-    </>
+    </main>
   );
 }
